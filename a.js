8539d4089bcbfd77e40ecfe2c56c27ed0dee245e
CastleRun <786901a@gmail.com>
2015-12-07 17:39:30 -0800

    Alpha 0.0
    Finished and ready for testing!

--- a.js
+++ a.js
@@ -1,8 +1,15 @@
 function load(id){
   var c = document.getElementById(id,loadAxes);
   var ctx = c.getContext("2d");
-  ctx["x"]=0;
-  ctx["y"]=0;
-  ctx["z"]=0;
   return ctx;
+}
+function clear(id,ctx){
+  c=document.getElementById(id);
+  ctx.fillStyle="#FFFFFF";
+  ctx.fillRect(0,0,c.width,c.height);
+}
+function line(ctx, x1, y1, z1, x2, y2, z2) {
+    ctx.moveTo((x1 / z1) + 100, 100 - ((y1 / z1) + 50));
+    ctx.lineTo((x2 / z2) + 100, 100 - ((y2 / z2) + 50));
+    ctx.stroke();
 }
