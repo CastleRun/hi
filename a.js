63c10e47cc9d45592e8f5716550d0bb49121cfef
CastleRun <786901a@gmail.com>
2015-12-16 12:44:06 -0800

    Alpha 0.0.3
    I checked w3schools.com.  This must work!

--- a.js
+++ a.js
@@ -13,5 +13,4 @@ function line(ctx, x1, y1, z1, x2, y2, z2) {
     ctx.moveTo((x1 / z1) + 100, 100 - ((y1 / z1) + 50));
     ctx.lineTo((x2 / z2) + 100, 100 - ((y2 / z2) + 50));
     ctx.stroke();
-    ctx.closePath();
 }
