07627598b99b913d7ecd733e81e84222d9dc6b3a
CastleRun <786901a@gmail.com>
2015-12-16 12:39:37 -0800

    Alpha 0.0.2
    Now, it must work!
    I checked a forum.

--- a.js
+++ a.js
@@ -9,7 +9,9 @@ function clear(id,ctx){
   ctx.fillRect(0,0,c.width,c.height);
 }
 function line(ctx, x1, y1, z1, x2, y2, z2) {
+    ctx.beginPath();
     ctx.moveTo((x1 / z1) + 100, 100 - ((y1 / z1) + 50));
     ctx.lineTo((x2 / z2) + 100, 100 - ((y2 / z2) + 50));
     ctx.stroke();
+    ctx.closePath();
 }
