59fbbf5309d5eed5826c25698953ccfcb96b7201
CastleRun <786901a@gmail.com>
2015-12-18 10:51:58 -0800

    0.2 Alpha
    Continued from 0.1 Alpha
    But it didn't work, so, the next day, CastleRun USED HIS BRAIN and came up with a great idea to fix the problem.
    Then, he committed the new version to Github.
    To be continued...

--- a.js
+++ a.js
@@ -1,16 +1,13 @@
-function Display(id){
-  var c = document.getElementById(id);
-  this = c.getContext("2d");
-  this.line= function(x1, y1, z1, x2, y2, z2) {
+function Hiize(ctx){
+  ctx["line"]= function(x1, y1, z1, x2, y2, z2) {
     this.beginPath();
     this.moveTo((x1 / z1) + 100, 100 - ((y1 / z1) + 50));
     this.lineTo((x2 / z2) + 100, 100 - ((y2 / z2) + 50));
     this.stroke();
-  }
-  this.clear= function(id,ctx){
+  };
+  ctx["clear"]= function(id,ctx){
     var c=document.getElementById(id);
     this.fillColor="#FFFFFF";
     this.fillRect(0,0,c.width,c.height);
-  }
+  };
 }
-
