ad1b4376c15fe893b1cd6f533ce6b9788aa5de65
CastleRun <786901a@gmail.com>
2015-12-24 14:52:47 -0800

    0.3.1a

--- a.js
+++ a.js
@@ -7,5 +7,5 @@ function Hiize(ctx,id){
   ctx["y2"]=0;
   ctx["z2"]=0;
   ctx["line"]="this.beginPath();this.moveTo((x1 / z1) + 100, 100 - ((y1 / z1) + 50));this.lineTo((x2 / z2) + 100, 100 - ((y2 / z2) + 50));this.stroke();";
-  ctx["clear"]="var c=document.getElementById(id);this.fillColor="#FFFFFF";this.fillRect(0,0,c.width,c.height);";
+  ctx["clear"]="var c=document.getElementById(id);this.fillColor='#FFFFFF';this.fillRect(0,0,c.width,c.height);";
 }
