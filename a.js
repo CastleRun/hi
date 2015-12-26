d239de9dce1a902364a7c7d0ec59975854a72cc0
CastleRun <786901a@gmail.com>
2015-12-26 12:18:34 -0800

    0.4a
    Developers: Back to the Prealpha
    (I don't read Warriors, but I will leave a Warriors reference anyway and the prealpha worked, so...)

--- a.js
+++ a.js
@@ -1,11 +1,11 @@
-function Hiize(ctx,id){
-  ctx["id"]=id;
-  ctx["x1"]=0;
-  ctx["y1"]=0;
-  ctx["z1"]=0;
-  ctx["x2"]=0;
-  ctx["y2"]=0;
-  ctx["z2"]=0;
-  ctx["line"]="this.beginPath();this.moveTo((x1 / z1) + 100, 100 - ((y1 / z1) + 50));this.lineTo((x2 / z2) + 100, 100 - ((y2 / z2) + 50));this.stroke();";
-  ctx["clear"]="var c=document.getElementById(id);this.fillColor='#FFFFFF';this.fillRect(0,0,c.width,c.height);";
+function line(ctx,x1,y1,z1,x2,y2,z2){
+  ctx.beginPath();
+  ctx.moveTo((x1 / z1) + 100, 100 - ((y1 / z1) + 50));
+  ctx.lineTo((x2 / z2) + 100, 100 - ((y2 / z2) + 50));
+  ctx.stroke();
+}
+function clear(id,ctx){
+  var c=document.getElementById(id);
+  ctx.fillColor='#FFFFFF';
+  ctx.fillRect(0,0,c.width,c.height);
 }
